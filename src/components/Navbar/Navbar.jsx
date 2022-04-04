a3c7581583a37f0897c284a41b68f71373c06519
gopimudumal99 <93375587+gopimudumal99@users.noreply.github.com>
2022-04-04 18:30:43 +0530

    hamberger add

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Toggle from "../Toggle/Toggle";
 import "./Navbar.css";
 import { Link } from "react-scroll";
+import Bar from "@iconscout/react-unicons/icons/uil-bars";
+import Cross from "@iconscout/react-unicons/icons/uil-times";
 function Navbar() {
+  const [hamberg, setHamberge] = useState(false);
+
+  const mobileView = () => {
+      setHamberge(() => !hamberg);
+  };
+
   return (
-    <div className="n-wrapper" id="Navbar">
+    <div className="n-wrapper" id="navbar">
       <div className="n-left">
         <div className="n-name">Gopi Mudumal</div>
+
         <Toggle />
       </div>
-      <div className="n-right">
+      <div  className="n-right active-right">
         <div className="n-list">
-          <ul>
+          <div className="bar" onClick={mobileView}>
+            {hamberg ? <Cross size={45} /> : <Bar size={45} />}
+          </div>
+          <ul style={{ visibility: hamberg ? "" : "hidden" }}>
             <li>
-              <Link activeClass="active" to="Navbar" spy={true} smooth={true}>
+              <Link to="navbar" spy={true} smooth={true}>
                 About
               </Link>
             </li>
