3d48ab4a99cf570a7cf7a293ee4125c64dec6028
gopimudumal99 <93375587+gopimudumal99@users.noreply.github.com>
2022-04-06 22:08:00 +0530

    added pic

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -4,7 +4,8 @@ import LinkdIn from "../../img/linkedin.png";
 import Instagram from "../../img/instagram.png";
 import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
-import boy from "../../img/boy.png";
+// import boy from "../../img/boy.png";
+import Gopi from '../../img/gopi.png'
 import thumbup from "../../img/thumbup.png";
 import Crown from "../../img/crown.png";
 import glassesimoji from "../../img/glassesimoji.png";
@@ -32,7 +33,7 @@ function Intro() {
           </span>
         </div>
         <Link to="contact" spy={true} smooth={true}>
-        <button className="button i-button">Hire me</button>
+          <button className="button i-button">Hire me</button>
         </Link>
         <div className="i-icons">
           <a href="https://github.com/gopimudumal99">
@@ -50,7 +51,7 @@ function Intro() {
       <div className="i-right">
         <img src={Vector1} alt="" />
         <img src={Vector2} alt="" />
-        <img src={boy} alt="" />
+        <img src={Gopi} alt="" />
         <motion.img
           initial={{ left: "-36%" }}
           whileInView={{ left: "-24%" }}
